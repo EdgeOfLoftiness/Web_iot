9baa608047bc13777c86645b6285de60fc9e62c6
EdgeOfLoftiness <104473137+EdgeOfLoftiness@users.noreply.github.com>
2023-12-14 03:37:25 -0300

    updated
    pagina actualizada

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 const iconoMenu = document.querySelector("#nav__responsive")
+const menu = document.querySelector(".nav__content")
 
-iconoMenu.addEventListener("click", (e) =>{
 
-    const menu = document.querySelector(".nav__content")
-
+iconoMenu.addEventListener("click", () =>{
     menu.classList.toggle("disabled")
 })
+
+window.addEventListener("resize", () =>{
+
+    menu.classList.add("disabled")
+
+    const width = window.innerWidth
+
+    if (width > 914 && menu.classList.contains("disabled"))
+    {
+        menu.classList.remove("disabled")
+    }
+
+})
+
